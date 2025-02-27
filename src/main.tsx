706054be0000435520f8d399498e7366667440a8
ryanlevee <ryanlevee@gmail.com>
2025-02-27 00:21:41 -0800

    netlify.toml, main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
-import './styles/app.css';
-import './styles/data.css';
-import './styles/events.css';
-import './styles/home.css';
-import './styles/index.css';
-import './styles/map.css';
-import './styles/sidebar.css';
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/app.css";
+import "./styles/data.css";
+import "./styles/events.css";
+import "./styles/home.css";
+import "./styles/index.css";
+import "./styles/map.css";
+import "./styles/sidebar.css";
 
-createRoot(document.getElementById('root')!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <App />
+    </StrictMode>,
+);
